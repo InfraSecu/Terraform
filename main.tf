a5b69aaab98355ed40e9f6c749eda4d047e44d75
InfraSecu <isi_paris_cpi_tdp329@supdevinci-edu.fr>
2025-04-28 03:19:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,14 +25,3 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Exemple volontairement vulnérable pour la démo
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  # Problème de sécurité: pas de chiffrement activé
-  # Problème de sécurité: accès public autorisé
-  
-}
