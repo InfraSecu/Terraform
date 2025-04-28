ed7ea2589f1ff4116c436d7559aa73a4d307ad61
InfraSecu <isi_paris_cpi_tdp329@supdevinci-edu.fr>
2025-04-28 02:05:30 +0200

    Create main.tf
    Premier test de déploiement

---
+++ main.tf
@@ -0,0 +1,38 @@
+# main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+    environment = "demo"
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Exemple volontairement vulnérable pour la démo
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # Problème de sécurité: pas de chiffrement activé
+  # Problème de sécurité: accès public autorisé
+  allow_blob_public_access = true
+}
