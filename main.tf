54bc6ab3f81b90925491d600e41c9782df4ebb01
InfraSecu <isi_paris_cpi_tdp329@supdevinci-edu.fr>
2025-04-29 10:09:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "example" {
   }
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "Reseau_utilitaire" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
