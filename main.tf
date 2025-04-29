e90830731ccb5c248374f2a2d73b6a5cc3d23a25
InfraSecu <isi_paris_cpi_tdp329@supdevinci-edu.fr>
2025-04-29 10:56:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# main.tf
 provider "azurerm" {
   features {}
 }
@@ -5,18 +6,21 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
+  tags = {
+    environment = "demo"
+  }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  
-  # Correction 1: Utilisation de la géo-réplication pour éviter la perte de données
-  account_replication_type = "GRS" # Géographiquement redondant
-  
-  # Correction 3: Forcer la version TLS minimum à 1.2
-  min_tls_version          = "TLS1_2"
-  
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
