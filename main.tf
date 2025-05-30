6a7a6f8c12ac7669f8aa5c13ec92b6cf0f30da7d
InfraSecu <isi_paris_cpi_tdp329@supdevinci-edu.fr>
2025-05-30 19:35:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,20 +10,12 @@ resource "azurerm_resource_group" "example" {
     environment = "demo"
   }
 }
-resource "azurerm_network_ddos_protection_plan" "example" {
-  name                = "example-protection-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.example.id
-    enable = true
-  }
 }
 
 resource "azurerm_subnet" "example" {
